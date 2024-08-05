fed2cb753a03d967d6070f6021f567aa17d85343
root <muneerahmed00916@gmail.com>
2024-08-06 01:17:16 +0530

    blog page added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import CreatePage from './pages/create'
 import PostPage from './pages/postPage'
 import { AppBar } from './components/app-bar'
 
