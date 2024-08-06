bc10beb01cb6b77fd52f86932ec7f5e68b96e47d
root <muneerahmed00916@gmail.com>
2024-08-07 02:31:46 +0530

    final

--- frontend/src/pages/create.tsx
+++ frontend/src/pages/create.tsx
@@ -1,4 +1,4 @@
-import React, { useState, ChangeEvent, useRef } from 'react';
+import React, { useState, ChangeEvent } from 'react';
 import { Image, Send } from "lucide-react";
 import { useNavigate } from 'react-router-dom';
 
